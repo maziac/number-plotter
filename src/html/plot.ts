4ee4d868060a430a3b429a278e699b102e2645bf
maziac <thomas.busse@gmx.net>
2021-05-03 18:30:11 +0200

    chart.js added.

--- src/html/plot.ts
+++ src/html/plot.ts
@@ -1,4 +1,6 @@
 
+declare var document: Document;
+declare var Chart: any;
 
 
 export class Plot {
@@ -6,6 +8,35 @@ export class Plot {
 
 	constructor(text: string) {
 		this.text = text;
+
+		const labels = [
+			'January',
+			'February',
+			'March',
+			'April',
+			'May',
+			'June',
+		];
+		const data = {
+			labels: labels,
+			datasets: [{
+				label: 'My First dataset',
+				backgroundColor: 'rgb(255, 99, 132)',
+				borderColor: 'rgb(255, 99, 132)',
+				data: [0, 10, 5, 2, 20, 30, 45],
+			}]
+		};
+
+		const config = {
+			type: 'line',
+			data,
+			options: {}
+		};
+
+		var myChart = new Chart(
+			document.getElementById('myChart'),
+			config
+		);
 	}
 
 }
